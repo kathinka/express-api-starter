01318f39f98602bf0f5c6c1f44f53d3dcd7d6849
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-05-14 20:40:35 +0200

    First commit:
     Update package.json and server.js:
    - Update package.json name and description
    - Add express-list-endpoints, mongodb, and mongoose dependencies
    - created routes and model for HappyThoughts
    - Imported and used router in server.js
    - Update mongoose connection and port configuration

---
+++ routes/Routes.js
@@ -0,0 +1,119 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import HappyThought from "../model/Happythoughts";
+const router = express.Router();
+
+//post new happy thought
+router.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newHappyThought = await new HappyThought({ message }).save();
+    res.status(201).json(newHappyThought);
+  } catch (error) {
+    console.error(error); // Log the error
+    res.status(400).json({
+      success: false,
+      response: error,
+      message: "Could not save thought",
+      error,
+    });
+  }
+});
+
+//get all happy thoughts, but only the 20 most recent
+router.get("/thoughts", async (req, res) => {
+  const thoughts = await HappyThought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
+
+//post a like to a happy thought
+router.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const updatedThought = await HappyThought.findByIdAndUpdate(
+      thoughtId,
+      { $inc: { hearts: 1 } },
+      { new: true }
+    );
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: "Thought not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Could not save like", error });
+  }
+});
+
+//delete a happy thought
+router.delete("/thoughts/:thoughtId", async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const deletedThought = await HappyThought.findByIdAndDelete(thoughtId);
+    if (deletedThought) {
+      res.json(deletedThought);
+    } else {
+      res.status(404).json({ message: "Thought not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Could not delete thought", error });
+  }
+});
+
+//filter happy thoughts by limit or skip or both
+router.get("/thoughts/filter", async (req, res) => {
+  const { limit, skip } = req.query;
+  const thoughts = await HappyThought.find()
+    .sort({ createdAt: "desc" })
+    .limit(parseInt(limit))
+    .skip(parseInt(skip))
+    .exec();
+  res.json(thoughts);
+});
+
+// get updated documentation
+router.get("/", (req, res) => {
+  try {
+    const endpoints = listEndpoints(router);
+    const updatedEndpoints = endpoints.map((endpoint) => {
+      if (endpoint.path === "/thoughts/filter") {
+        return {
+          path: endpoint.path,
+          methods: endpoint.methods,
+          queryParameters: [
+            {
+              name: "limit",
+              description:
+                "filter the thoughts by the number of thoughts you want to get Example: /thoughts/filter?limit=5  you could also combine limit and skip Example: /thoughts/filter?limit=5&skip=5",
+            },
+            {
+              name: "skip",
+              description:
+                "filter the thoughts by the number of thoughts you want to skip Example: /thoughts/filter?skip=5  you could also combine limit and skip Example: /thoughts/filter?limit=5&skip=5",
+            },
+          ],
+        };
+      }
+      return {
+        path: endpoint.path,
+        methods: endpoint.methods,
+      };
+    });
+    res.json(updatedEndpoints);
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const customError = new Error(
+      "An error occurred while fetching the endpoints"
+    );
+    res.status(404).json({ message: customError.message });
+  }
+});
+
+router.use("/", (req, res) => {
+  res.send(listEndpoints(router));
+});
+
+export default router;
